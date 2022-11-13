0fafafab2b86eefd6de67dca1056e7fbc83033cd
Miguel Quiaios <miguelquiaios12@gmail.com>
2022-11-13 20:18:37 +0000

    lab solved

--- index.js
+++ index.js
@@ -4,11 +4,21 @@ const mongoose = require('mongoose');
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
+const {deleteOne} = require('./models/Recipe.model');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
 //Method 1 : Using Async Await
-//const firstRecipe = data[0];
+const carbonara = {
+  title:'carbonara',
+  level: 'Amateur',
+  ingredients:['massa', 'bacon', 'queijo', 'natas'],
+  cousine: 'italiano',
+  dishType: 'main_course',
+  image: 'https://veja.abril.com.br/wp-content/uploads/2021/04/CARBONARA-CYBERCOOK-1.jpeg.jpg',
+  duration: 50,
+  creator: 'Miguel Quiaios'
+}
 
 const manageRecipes = async () => {
   try {
@@ -18,34 +28,27 @@ const manageRecipes = async () => {
 
     // Before adding any recipes to the database, let's remove all existing ones
     await Recipe.deleteMany();
-   // await Recipe.create(firstRecipe)
-    await Recipe.insertMany(data)
-    //console.log(firstRecipe.title)
+    await Recipe.create('carbonara');
+    console.log(carbonara.title)
+
+    let totalRecipes = await Recipe.insertMany(data);
+
+
+    for(let i = 0; i < totalRecipes; i++){
+      console.log(totalRecipes[i].title)
+    }
+
+
+    let updateGenovese = await Recipe.findOneAndUpdate({title: 'Rigatoni alla Genovese'}, {duration: 100});
+    console.log(updateGenovese)
+    console.log("duration change") 
  
+     await Recipe.deleteOne({title: 'Carrot Cake'})
+     
+     dbConnection.disconnect();
 
 
-
-    // Run your code here, after you have insured that the connection was made
-  } catch (error) {
+} catch (error) {
     console.log(error);
   }
 };
-
-manageRecipes();
-
-//Method 2: Using .then() method
-//If you want to use this method uncomment the code below:
-
-/* mongoose
-  .connect(MONGODB_URI)
-  .then((x) => {
-    console.log(`Connected to the database: "${x.connection.name}"`);
-    // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany();
-  })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
-  })
-  .catch((error) => {
-    console.error('Error connecting to the database', error);
-  }); */
